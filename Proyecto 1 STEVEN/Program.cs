08f433cc5a88e60423d8a8a69200da8c8995db29
Steventg2002 <109814022+Steventg2002@users.noreply.github.com>
2022-08-02 00:04:19 -0300

    Add files via upload
    TAREA 1

---
+++ Proyecto 1 STEVEN/Program.cs
@@ -0,0 +1,9 @@
+﻿//Declaración de variables
+int num1 = 9;
+int num2 = 9;
+var total = num1 + num2;
+string name = "Steven";
+int edad = 19;
+//Impreción de datos
+Console.WriteLine("La suma es: " + total);
+Console.WriteLine(name + " " + edad);
