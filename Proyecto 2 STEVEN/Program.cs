43b45f89fbc1684f5f6fb5264bcb0745fe7a4aa7
Steventg2002 <109814022+Steventg2002@users.noreply.github.com>
2022-08-02 00:07:48 -0300

    Add files via upload
    Tarea 2

---
+++ Proyecto 2 STEVEN/Program.cs
@@ -0,0 +1,8 @@
+﻿//Declaración de variables
+string firstname = "Steven";
+String lastname = "Adonay";
+int edad = 19;
+//Concatenación de variables
+var name = firstname + " " + lastname;
+//Imprimir variables dentro del texto
+Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
